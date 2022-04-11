4d4c24982a3e836484c43bf5c121df475286382e
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-04-11 11:12:35 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,78 @@
-export const App = () => {
+import React, { useState, useEffect } from 'react';
+import Form from './Form/Form';
+import Filter from './Filter/Filter';
+import NameList from './NameList/NameList';
+import shortid from 'shortid';
+import store from '../redux/phonebook-reduser';
+import * as actions from '../redux/phonebook-action'
+
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleSubmitForm = (name, number) => {
+    const checkname = contacts.find(contact =>
+      contact.name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    store.dispatch(actions.addContact(name, number));
+
+    if (!checkname) {
+      const contact = {
+        id: shortid.generate(),
+        name: name,
+        number: number,
+      };
+      
+      
+      
+
+      setContacts([contact, ...contacts]);
+    } else {
+      alert(name + ' is already in contact list');
+    }
+  };
+
+  const getVisibleContacts = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  const visibleContacts = getVisibleContacts();
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const deleteContact = contactId => {
+    store.dispatch(actions.deleteContact(contactId));
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  }
+  
+
+  useEffect(() => {
+    if (localStorage.getItem('contacts')) {
+      const currentContactList = JSON.parse(localStorage.getItem('contacts'));
+      setContacts(currentContactList);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <h1>Phonebook</h1>
+      <Form onSubmit={handleSubmitForm} />
+
+      <h2>Contacts</h2>
+      <Filter changeFilter={changeFilter} filter={filter} />
+      <NameList
+        visibleContacts={visibleContacts}
+        onDeleteContact={deleteContact}
+      />
+    </>
   );
-};
+}
