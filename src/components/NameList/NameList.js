d942abf5bdac51bdfffc17196af2eae28162bf8f
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-04-12 11:43:18 +0300

    Update NameList.js

--- src/components/NameList/NameList.js
+++ src/components/NameList/NameList.js
@@ -2,15 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import NameItem from '../NameItem/NameItem';
 import styles from './nameList.module.css';
+import * as actions from '../../redux/phonebook-action';
+import { connect } from 'react-redux';
 
 const NameList = ({ visibleContacts, onDeleteContact }) => (
   <ul className={styles.list}>
-    {visibleContacts.map(({ id, name, number }) => (
+    {visibleContacts && visibleContacts.map(({ id, name, number }) => (
       <NameItem
         id={id}
         name={name}
         number={number}
-        onDeleteContact={onDeleteContact}
+        onDeleteContact={() => onDeleteContact(id)}
       />
     ))}
   </ul>
@@ -21,4 +23,12 @@ NameList.propTypes = {
   onDeleteContact: PropTypes.func,
 };
 
-export default NameList;
+const mapStatetoProps = state => ({
+  visibleContacts: state.contactReduser,
+});
+
+const mapDispatchToProps = dispatch => ({
+  onDeleteContact: id => dispatch(actions.deleteContact(id)),
+});
+
+export default connect(mapStatetoProps, mapDispatchToProps)(NameList);
