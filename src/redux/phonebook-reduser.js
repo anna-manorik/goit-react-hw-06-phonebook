4119cbe814898309603949c0202573a76b181bfb
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-04-12 11:43:26 +0300

    Update phonebook-reduser.js

--- src/redux/phonebook-reduser.js
+++ src/redux/phonebook-reduser.js
@@ -1,25 +1,52 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import shortid from 'shortid';
 
-const pbReduser = (state = {}, { type, payload }) => {
-switch(type){
-    case 'ADD_CONTACT':
-        console.log('add contact', payload);
-        break;
+const contactReduser = (state = [], { type, payload }) => {
+  switch (type) {
+    case 'ADD_CONTACT': {
+      const checkname = state.find(contact =>
+        contact.name.toLowerCase().includes(payload.name.toLowerCase())
+      );
 
-    case 'DELETE_CONTACT':
-        console.log('delete contact', payload);
-        break;
+      if (!checkname) {
+        const contact = {
+          id: shortid.generate(),
+          name: payload.name,
+          number: payload.number,
+        };
+
+        return [contact, ...state];
+      } else {
+        alert(payload.name + ' is already in contact list');
+      }
+
+      break;
+    }
+
+    case 'DELETE_CONTACT': {
+      return state.filter(contact => contact.id !== payload);
+    }
 
     default:
-        return state;
-
-}
-
-
+      return state;
+  }
 };
 
-const store = createStore(pbReduser);
+const filterReduser = (state = '', { type, payload }) => {
+  switch (type) {
+    case 'CHANGE_FILTER':
+      return payload;
+
+    default:
+      return state;
+  }
+};
+
+const rootReduser = combineReducers({
+  contactReduser,
+  filterReduser,
+});
+
+const store = createStore(rootReduser);
 
 export default store;
-
-
