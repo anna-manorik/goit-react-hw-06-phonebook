e3f0833e77b595c2fb2e1402fb19ef0534c6a892
anna-manorik <86349455+anna-manorik@users.noreply.github.com>
2022-04-13 16:31:58 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,32 +7,6 @@ import * as actions from '../redux/phonebook-action';
 import { connect } from 'react-redux';
 
 function App() {
-  // const [contacts, setContacts] = useState([]);
-  // const [filter, setFilter] = useState('');
-
-  // const getVisibleContacts = () => {
-  //   return contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(filter.toLowerCase())
-  //   );
-  // };
-
-  // const visibleContacts = getVisibleContacts();
-
-  // const changeFilter = e => {
-  //   setFilter(e.currentTarget.value);
-  // };
-
-  // useEffect(() => {
-  //   if (localStorage.getItem('contacts')) {
-  //     const currentContactList = JSON.parse(localStorage.getItem('contacts'));
-  //     setContacts(currentContactList);
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
-
   return (
     <>
       <h1>Phonebook</h1>
